81ec7744395c8ec4b3f4e0c545e9dbe587d74df8
Laurent THIEBAULT <thiebault.laurent@gmail.com>
2018-02-03 18:14:23 +0100

    wip

--- src/renderer/components/modals/create-note-modal/CreateNoteModal.vue
+++ src/renderer/components/modals/create-note-modal/CreateNoteModal.vue
@@ -2,58 +2,67 @@
 </template>
 
 <script>
-import editor from '../../editor/Editor';
-import languages from '../../../assets/data/languages.json';
+  import editor from '../../editor/Editor';
+  import languages from '../../../assets/data/languages.json';
 
-export default {
-  name: 'cn-create-note-modal',
-  components: {
-    editor,
-  },
-  data() {
-    return {
-      note: {
-        name: '',
-        description: '',
-        files: {},
-        updatedAt: null,
-        createdAt: null
+  export default {
+    name: 'cn-create-note-modal',
+    components: {
+      editor,
+    },
+    data() {
+      return {
+        note: {
+          name: '',
+          description: '',
+          files: {},
+          updatedAt: null,
+          createdAt: null
+        },
+        files: [{
+          name: '',
+          language: 'text',
+          content: ''
+        }],
+        languages
+      };
+    },
+    mounted() {
+      this.$refs.noteName.focus();
+    },
+    methods: {
+      createNote() {
+        this.files.forEach(file => {
+          this.note.files[file.name] = file;
+        });
+
+        this.note.createdAt = new Date();
+        this.note.updatedAt = new Date();
+
+        this.$store.dispatch('addNote', this.note).then(() => {
+          this.$parent.close();
+        });
       },
-      files: [{
-        name: '',
-        language: 'text',
-        content: ''
-      }],
-      languages
-    };
-  },
-  mounted() {
-    this.$refs.noteName.focus();
-  },
-  methods: {
-    createNote() {
-      this.files.forEach(file => {
-        this.note.files[file.name] = file;
-      });
-
-      this.note.createdAt = new Date();
-      this.note.updatedAt = new Date();
-
-      this.$store.dispatch('addNote', this.note).then(() => {
-        this.$parent.close();
-      });
+      addFile() {
+        this.files.push({
+          name: '',
+          language: 'text',
+          content: ''
+        });
+      },
+      deleteFile(file) {
+        this.files = this.files.filter(f => f !== file);
+      }
     },
-  },
-  computed: {
-    isDisabled() {
-      return (
-        !/\S/.test(this.note.name) ||
-        !/\S/.test(this.note.language) ||
-        !/\S/.test(this.note.content)
-      );
+    computed: {
+      isDisabled() {
+        return (
+          !/\S/.test(this.note.name) ||
+          this.files.some(file => !/\S/.test(file.name) || !/\S/.test(file.language) || !/\S/.test(file.content))
+        );
+      },
     },
-  },
-};
+  };
 </script>
 
 <style src="./CreateNoteModal.scss" lang="scss">
